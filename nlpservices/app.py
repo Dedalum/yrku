a11ede44d01c0b83ac1058ae5d2709707bff9793
Dedalum <ha.lagrange9000@gmail.com>
2019-11-27 13:40:37 +0100

    Update main and rename it to app.py

--- nlpservices/app.py
+++ nlpservices/app.py
@@ -20,29 +20,35 @@ def recommend_handler(book_title):
     Returns:
         string: JSON list of top books recommended
     """
-    top_books = app.config['RECOMMENDER'].top_books(flask.escape(book_title))
+    try:
+        use_author = True if flask.request.args.get(
+            "use_author") == "true" else False
+        top_books = app.config["RECOMMENDER"].top_books(
+            flask.escape(book_title), use_author=use_author)
+    except Exception as err:
+        return resource_not_found(err)
 
     return nlpservice.recommend.Recommender.jsonify_recommender_result(
         top_books), 200
 
 
 @app.errorhandler(404)
-def resource_not_found(e):
+def resource_not_found(err):
     """Error 404 handler"""
-    return flask.jsonify(error=str(e)), 404
+    return flask.jsonify(error=str(err)), 404
 
 
 def main():
     """main function"""
     config = configparser.ConfigParser()
-    config.read_string("config.ini")
+    config.read("config.ini")
 
     recommender = nlpservice.recommend.Recommender(config["mysql"]["host"],
                                                    config["mysql"]["user"],
                                                    config["mysql"]["password"],
                                                    config["mysql"]["db"])
     recommender.pre_setup()
-    ideal_nb_topics = recommender.ideal_nb_topics(5, 20)
+    ideal_nb_topics = 13  # recommender.ideal_nb_topics(5, 15)
     recommender.setup(nb_topics=ideal_nb_topics)
 
     app.config['RECOMMENDER'] = recommender

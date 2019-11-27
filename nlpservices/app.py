efca8d697238a4e64a64b7716a3d1f2a3ee1b6ad
Dedalum <ha.lagrange9000@gmail.com>
2019-11-27 17:45:01 +0100

    Add config for Flask app host

--- nlpservices/app.py
+++ nlpservices/app.py
@@ -52,7 +52,7 @@ def main():
     recommender.setup(nb_topics=ideal_nb_topics)
 
     app.config['RECOMMENDER'] = recommender
-    app.run()
+    app.run(host=config["app"]["host"])
 
 
 if __name__ == "__main__":

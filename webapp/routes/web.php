07fadb249f991a9aabdf49dff8dc11fd50ea7862
Dedalum <ha.lagrange9000@gmail.com>
2019-11-30 18:20:59 +0100

    Update routes

--- webapp/routes/web.php
+++ webapp/routes/web.php
@@ -3,38 +3,9 @@
 use App\Book;
 use Illuminate\Http\Request;
 
-// display all books
-Route::get('/', function () {
-    $books = Book::orderBy('author')->get();
-
-    return view('books', ['books' => $books]);
+Route::any('/', function () {
+    return View::make('home');
 });
-
-
-// add new book
-Route::post('/book', function (Request $request) {
-    $validator = Validator::make($request->all(), [
-        'title' => 'required|max:255',
-        'author' => 'required|max:255',
-    ]);
-
-    if ($validator->fails()) {
-        return redirect('/')
-            ->withInput()
-            ->withErrors($validator);
-    }
-
-    $book = new Book;
-    $book->title = $request->title;
-    $book->author = $request->author;
-    $book->save();
-
-    return redirect('/');
-});
-
-// delete a book
-Route::delete('/book/{id}', function ($id) {
-    Book::findOrFail($id)->delete();
-
-    return redirect('/');
-});
+Route::resource('books', 'BookController');
+Route::get('/book/{id}', 'BookController@show');
+Route::delete('/book/{id}', 'BookController@destroy');

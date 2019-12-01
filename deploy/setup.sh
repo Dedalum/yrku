c30cdeeb9e629cf4cf73210338f15c2336d60699
Dedalum <ha.lagrange9000@gmail.com>
2019-12-01 13:58:50 +0100

    Update deploy variables file .env and the corresponding config file

---
+++ deploy/setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -u
+
+ENV_VAR_FILE=".env"
+
+# Load env vars to use with envsubst
+export $(grep -v '^#' ${ENV_VAR_FILE} | xargs)
+
+# Make sure necessary directories are created
+mkdir -p ${MYSQL_DATA_DIR} ${MYSQL_CONF_DIR} ${MYSQL_ENTRYPOINT_DIR}
+mkdir -p ${NLP_RECOMMEND_DATA_DIR} ${NLP_RECOMMEND_CONF_DIR}
+mkdir -p ${WEBAPP_DIR} ${WEBAPP_CONF_DIR}
+mkdir -p ${NGINX_DIR} ${NGINX_CONFD_DIR} ${NGINX_LOG_DIR}
+
+# Set all variables using the config env var file .env
+envsubst < ./mysql/script.sql.example > "${MYSQL_ENTRYPOINT_DIR}/script.sql"
+envsubst < ./nlpservices/recommend/config.ini.example > "${NLP_RECOMMEND_CONF_DIR}/config.ini"
+envsubst < ./webapp/.env.example > "${WEBAPP_DIR}/.env"
+envsubst < ./webapp/config/nlp_api.php.example > "${WEBAPP_CONF_DIR}/nlp_api.php"
+
+

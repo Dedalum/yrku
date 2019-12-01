b7f6a6445b0dc9f9d38d9c35bb27306c4f62288a
Dedalum <ha.lagrange9000@gmail.com>
2019-12-01 19:46:11 +0100

    Fix setup issue with nginx config file and envsubstr

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -18,6 +18,10 @@ envsubst < ./mysql/script.sql.example > "${MYSQL_ENTRYPOINT_DIR}/script.sql"
 envsubst < ./nlpservices/recommend/config.ini.example > "${NLP_RECOMMEND_CONF_DIR}/config.ini"
 envsubst < ./webapp/.env.example > "${WEBAPP_DIR}/.env"
 envsubst < ./webapp/config/nlp_api.php.example > "${WEBAPP_CONF_DIR}/nlp_api.php"
-envsubst < ./nginx/conf.d/webapp.conf.example > "${NGINX_CONFD_DIR}/webapp.conf"
+
+# nginx uses variables with '$' sign which collides with our own variables
+# we avoid that by passing to envsubst the list of variables to replace
+vars_to_replace=$(grep -P "\\$\\{[A-Z_]*\\}" nginx/conf.d/webapp.conf.example -o)
+envsubst "$vars_to_replace" < ./nginx/conf.d/webapp.conf.example > "${NGINX_CONFD_DIR}/webapp.conf"
 
 

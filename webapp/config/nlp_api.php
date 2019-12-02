69eefb0824b67c0c3669adeaabb2a252afa2513f
Dedalum <ha.lagrange9000@gmail.com>
2019-12-02 15:29:33 +0100

    Add missing documentation to webapp

--- webapp/config/nlp_api.php
+++ webapp/config/nlp_api.php
@@ -1,4 +1,13 @@
 <?php
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | NLP API host
+    |--------------------------------------------------------------------------
+    |
+    | This value is the host:port of the NLP API to query recommended books
+    | from.
+    */
+ 
     'host' => 'http://localhost:5000'
 ];

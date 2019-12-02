f3bd96efc57ac454ff4f176260eb40dc1eb37e78
Dedalum <ha.lagrange9000@gmail.com>
2019-12-02 14:38:35 +0100

    Clean-up tests

--- nlpservices/tests/test_recommend.py
+++ nlpservices/tests/test_recommend.py
@@ -1,9 +1,7 @@
 """Test for helpers functions"""
-import os
 import shutil
 
 import pytest
-from gensim.test.utils import common_corpus, common_dictionary, common_texts
 
 import nlpservice.helpers
 import nlpservice.recommend
@@ -30,44 +28,44 @@ class TestRecommender:
         it to files, then load it from the same files.
         """
         # pre-setup
-        recommender = nlpservice.recommend.Recommender("",
-                                                       "",
-                                                       "",
-                                                       "",
-                                                       data_dir=self.data_dir)
-        recommender.pre_setup(docs=self.books)
+        try:
+            recommender = nlpservice.recommend.Recommender(
+                "", "", "", "", data_dir=self.data_dir)
+            recommender.pre_setup(docs=self.books)
 
-        # Load from files pre-setup
-        recommender.pre_setup(docs=self.books)
+            # Load from files pre-setup
+            recommender.pre_setup(docs=self.books)
+        except Exception as err:
+            pytest.fail("failed test: %s" % err)
 
     def test_ideal_nb_topics(self):
         """"
         Test finding the ideal number of topics to be used in our LSI model.
         """
         # pre-setup
-        recommender = nlpservice.recommend.Recommender("",
-                                                       "",
-                                                       "",
-                                                       "",
-                                                       data_dir=self.data_dir)
-        recommender.pre_setup(docs=self.books)
+        try:
+            recommender = nlpservice.recommend.Recommender(
+                "", "", "", "", data_dir=self.data_dir)
+            recommender.pre_setup(docs=self.books)
 
-        ideal_nb_topics = recommender.ideal_nb_topics(2, 15)
-        assert 15 >= ideal_nb_topics >= 2
+            ideal_nb_topics = recommender.ideal_nb_topics(2, 15)
+            assert 15 >= ideal_nb_topics >= 2
+        except Exception as err:
+            pytest.fail("failed test: %s" % err)
 
     def test_setup(self):
         """
         Test loading the LSI model, the vectors and the similarities matrix,
         saving it to files and then reload these from the same files.
         """
-        recommender = nlpservice.recommend.Recommender("",
-                                                       "",
-                                                       "",
-                                                       "",
-                                                       data_dir=self.data_dir)
-        recommender.pre_setup(docs=self.books)
+        try:
+            recommender = nlpservice.recommend.Recommender(
+                "", "", "", "", data_dir=self.data_dir)
+            recommender.pre_setup(docs=self.books)
 
-        recommender.setup()
+            recommender.setup()
+        except Exception as err:
+            pytest.fail("failed test: %s" % err)
 
     def test_top_books(self):
         """
@@ -76,31 +74,27 @@ class TestRecommender:
         # Pre-setup, optimize the number of topics and final setup
         # for the test, we do not use MySQL to load the data here so fill it
         # with empty strings
-        recommender = nlpservice.recommend.Recommender("",
-                                                       "",
-                                                       "",
-                                                       "",
-                                                       data_dir=self.data_dir)
-        recommender.pre_setup(docs=self.books)
-        ideal_nb_topics = recommender.ideal_nb_topics(2, 15)
-        recommender.setup(nb_topics=ideal_nb_topics)
+        try:
+            recommender = nlpservice.recommend.Recommender(
+                "", "", "", "", data_dir=self.data_dir)
+            recommender.pre_setup(docs=self.books)
+            ideal_nb_topics = recommender.ideal_nb_topics(2, 15)
+            recommender.setup(nb_topics=ideal_nb_topics)
 
-        title = "French literature, poems and other arts in 19th cent."
-        top_books = recommender.top_books(title, top_nb_docs=3)
+            title = "French literature, poems and other arts in 19th cent."
+            top_books = recommender.top_books(title, top_nb_docs=3)
 
-        print("Top books for %s" % title)
-        for book in top_books:
-            print(book)
+            print("Top books for %s" % title)
+            for book in top_books:
+                print(book)
 
-    def test_top_books_use_author(self):
-        """
-        Test top books recommendation, using the title and the author
-        Requires a MySQL DB running as we query the books and author from it.
-        """
-        # TODO
+        except Exception as err:
+            pytest.fail("failed test: %s" % err)
+
     @pytest.fixture(scope="session", autouse=True)
     def clean_up(self, request):
+        """Clean up created files and directories for testing purposes"""
         def _end():
-            shutil.rmtree(self.data_dir)  # clean-up...
+            shutil.rmtree(self.data_dir)
 
         request.addfinalizer(_end)
